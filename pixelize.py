bce2adc9da16a31527a0d87a241f1d4a446c904f
semenishchev-ai <113626930+semenishchev-ai@users.noreply.github.com>
2022-12-18 23:25:54 +0300

    New version
    Algorithm changed. Partial instruction

--- pixelize.py
+++ pixelize.py
@@ -1,103 +1,83 @@
-from PIL import Image
 from fpdf import FPDF
+import numpy as np
+from PIL import Image
 
 
-# following subfunctions are made for different types of picture's modifying
-def av_geom(colors):  # colored
-    sm0 = 1
-    sm1 = 1
-    sm2 = 1
-    for x in colors:
-        sm0 *= x[0] if x[0] else 1
-        sm1 *= x[1] if x[1] else 1
-        sm2 *= x[2] if x[2] else 1
-    return (int((sm0) ** (1 / len(colors))), int((sm1) ** (1 / len(colors))), int((sm2) ** (1 / len(colors))))
+def fs_dither(img, nc):
+    # Floyd-Steinberg dither the image into a palette with color_number colours per
+    # channel.
+    arr = np.array(img, float) / 255
+    width, height = img.size
+    for i in range(height):
+        for j in range(width):
+            old_val = arr[i, j].copy()
+            new_val = np.round(old_val * (nc - 1)) / (nc - 1)
+            arr[i, j] = new_val
+            err = old_val - new_val
+            if j < width - 1:
+                arr[i, j + 1] += err * 7 / 16
+            if i < height - 1:
+                if j > 0:
+                    arr[i + 1, j - 1] += err * 3 / 16
+                arr[i + 1, j] += err * 5 / 16
+                if j < width - 1:
+                    arr[i + 1, j + 1] += err / 16
+    carr = np.array(arr / np.max(arr, axis=(0, 1)) * 255, dtype=np.uint8)
+    return Image.fromarray(carr)
 
 
-def av_arifm(colors):
-    sm0 = 0
-    sm1 = 0
-    sm2 = 0
-    for x in colors:
-        sm0 += x[0]
-        sm1 += x[1]
-        sm2 += x[2]
-    return (sm0 // len(colors), sm1 // len(colors), sm2 // len(colors))
+def back_to_size(img, pix_size):
+    width, height = img.size
+    new = Image.new("RGB", (width * pix_size, height * pix_size))
+    for i in range(height * pix_size):
+        for j in range(width * pix_size):
+            new.putpixel((j, i), img.getpixel((j // pix_size, i // pix_size)))
+    return new
 
 
-def basic(colors):  # first variant
-    return colors[len(colors) // 2]
-
-
-# subfunctin for comparising
-def dist(x1, x2):
-    return (x1[2] - x2[2]) ** 2 + (x1[1] - x2[1]) ** 2 + (x1[0] - x2[0]) ** 2
-
-
-def cosine_sqr(x1, x2):
-    return (x1[0] * x2[0] + x1[1] * x2[1] + x1[2] * x2[2]) ** 2 / (dist(x1, (0, 0, 0)) * dist(x2, (0, 0, 0)))
-
-
-# functions for changing cur_color to one of the basic's
-def change(pix, b_colors):
-    x = [dist(k, pix) for k in b_colors.keys()]
-    return list(b_colors.keys())[x.index(min(x))]
+def count_pixels(img, basic_colors, pixel_size):
+    width, height = img.size
+    for i in range(height):
+        for j in range(width):
+            basic_colors[img.getpixel((j, i))][1] += 1
+    for x in list(basic_colors.values()):
+        x[1] //= pixel_size ** 2
 
 
 class PDF(FPDF):
     def header(self):
-        self.set_font("Arial", 'B', 20)
+        self.set_font("Arial", 'B', size=20)
         self.cell(0, 10, "Intruction", ln=True, align='C')
 
 
-# main service funtion
-def pixelize(im, pix_size, basic_colors):
+def write_the_instruction(path, new_img, basic_colors):
     fpdf = PDF('P', 'mm', 'Letter')
+    fpdf.set_font("Arial", size=16)
     fpdf.add_page()
-    instr = {}
-    n, m = im.size[0], im.size[1]
-    res = Image.new("RGB", (n, m), color=0)
-    for i in range(0, n, pix_size):
-        for j in range(0, m, pix_size):
-            points = {}
-            for x in range(pix_size):
-                if i + x >= n:
-                    break
-                for y in range(pix_size):
-                    if j + y >= m:
-                        continue
-                    points[(i + x, j + y)] = im.getpixel((i + x, j + y))
-            colors = list(sorted(points.values()))
-            color = change(av_arifm(colors), basic_colors)
-            if color in instr:
-                instr[color][0].append((i // pix_size, j // pix_size))
-                instr[color][1] += 1
-            else:
-                instr[color] = [[], 0]
-            for point in points.keys():
-                res.putpixel(point, color)
-    for x in instr.keys():
-        fpdf.set_font("Arial", size=16)
-        fpdf.cell(0, 10, f"{basic_colors[x]}: {instr[x][1]}", ln=True)
-    for x in instr.keys():
-        fpdf.set_font("Arial", 'B', size=16)
-        fpdf.cell(0, 10, f"{basic_colors[x]} squares are:", ln=True, align='C')
-        fpdf.set_font("Arial", size=16)
-        fpdf.multi_cell(0, 20, f"{str((instr[x][0]))[1:-1]}")
-    fpdf.output("output.pdf")
-    # res.show()
-    return res
+    for x in list(basic_colors.values()):
+        fpdf.cell(0, 10, f"{x[0]}: {x[1]}", ln=True)
+    fpdf.image(path, x=15, y=105, w=90)
+    fpdf.image("output.png", x=115, y=105, w=90)
+    fpdf.output(f"{path[:-4]}_instruction.pdf")
+    return f"{path[:-4]}_instruction.pdf"
 
 
-# starts the process
 def process():
-    image = Image.open("img.jpg")  # choose the image
-    pixel_size = 20  # set pixel_size
-    basic_colors = {(0, 0, 0): "Black", (255, 255, 255): "White", (255, 0, 0): "Red", (255, 255, 0): "Yellow",
-                    (0, 255, 0): "Green", (0, 255, 255): "Heaven", (0, 0, 255): "Blue",
-                    (255, 0, 255): "Violet"}  # in work
-    new_image = pixelize(image, pixel_size, basic_colors)
-    new_image.save("output.jpg")
+    path = input()  # path to image input
+    image = Image.open(path)
+    pixel_size = 4  # set pixel_size
+    number_of_pins = 360  # set how many pins on lines (multiplied by pixel_size)
+    # new_image = image.resize((number_of_pins, number_of_pins * image.size[1] // image.size[0]))
+    basic_colors = {(0, 0, 0): ["Black", 0], (255, 255, 0): ["Yellow", 0], (0, 255, 255): ["Cyan", 0],
+                    (255, 0, 255): ["Magenta", 0], (255, 0, 0): ["Red", 0], (0, 255, 0): ["Green", 0],
+                    (0, 0, 255): ["Blue", 0], (255, 255, 255): ["White", 0]}  # palette
+    new_image = image.resize((image.size[0] // pixel_size, image.size[1] // pixel_size))
+    new_image = fs_dither(new_image, 2)
+    new_image = back_to_size(new_image, pixel_size)
+    # new_image.show()
+    new_image.save("output.png")
+    count_pixels(new_image, basic_colors, pixel_size)
+    return write_the_instruction(path, new_image, basic_colors)
 
 
 process()
