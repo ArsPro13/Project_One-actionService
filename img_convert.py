07d225be7a4ecea69e2a27c395c133552ad3d8a5
semenishchev-ai <113626930+semenishchev-ai@users.noreply.github.com>
2022-12-20 23:09:53 +0300

    Full instruction

--- img_convert.py
+++ img_convert.py
@@ -7,19 +7,36 @@ def write_the_instruction(path, new_img, basic_colors, pixel_size):
     fpdf = FPDF()
     fpdf.add_page()
     fpdf.set_font("Arial", 'B', size=20)
-    fpdf.cell(0, 10, "Instruction", ln=True, align='C')
+    fpdf.cell(0, 10, "Pin art", ln=True, align='C')
+    fpdf.cell(0, 10, "instruction", ln=True, align='C')
     fpdf.set_font("Arial", size=16)
-    for x in list(basic_colors.values()):
-        fpdf.cell(0, 10, f"{x[0]}: {x[1]}", ln=True)
-    fpdf.image(path, x=15, y=105, w=90)
-    fpdf.image("output.png", x=115, y=105, w=90)
+    for x in range(4):
+        fpdf.text(65, 10 * x + 50, f"{list(basic_colors.values())[x + 4][0]}: {list(basic_colors.values())[x + 4][1]}")
+        fpdf.text(110, 10 * x + 50, f"{list(basic_colors.values())[x][0]}: {list(basic_colors.values())[x][1]}")
+    fpdf.image(path, x=10, y=130, w=90)
+    fpdf.image("output.png", x=110, y=130, w=90)
     width, height = new_img.size
+    fpdf.add_page()
+    k = 0
+    fpdf.set_font("Arial", 'B', size=20)
+    fpdf.cell(0, 10, "Fragmentation", ln=True, align='C')
+    fpdf.set_font("Arial", size=10)
     for i in range(0, width, 30 * pixel_size):
         for j in range(0, height, 40 * pixel_size):
+            k += 1
+            a, b = min(30 * pixel_size + i, width) * 190 / width, min(40 * pixel_size + j, height) * 190 / width
+            fpdf.rect(10, 25, a, b)
+            x = (min(30 * pixel_size + i, width) - i) / 2
+            y = (min(40 * pixel_size + j, height) - j) / 2
+            fpdf.text(8 + a - (190 * x / width), 24 + b - (190 * y / width), str(k))
+    fpdf.set_font("Arial", size=18)
+    k = 0
+    for i in range(0, width, 30 * pixel_size):
+        for j in range(0, height, 40 * pixel_size):
+            k += 1
             fpdf.add_page()
             fpdf.cell(0, 15,
-                      f"Rectange ({i // pixel_size}, {j // pixel_size}) - ({min(30 + i // pixel_size, width)}, {min(40 + j // pixel_size, height)})",
-                      align='C')
+                      f"Rectangle {k}", align='C')
             for x in range(0, 30 * pixel_size, pixel_size):
                 for y in range(0, 40 * pixel_size, pixel_size):
                     if (x + i < width) and (y + j < height):
