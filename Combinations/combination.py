ce5a9512ce60953407f88de6cd2ef3dfa40165fa
Kristina2802 <krissti200483@gmail.com>
2022-12-21 09:01:25 +0300

    Improve combination function

--- Combinations/combination.py
+++ Combinations/combination.py
@@ -1,11 +1,38 @@
 import random
+import csv
 
 def combination():
-    f = open('ingredients_after_change.txt', 'r')
-    ingredients = []
-    for line in f:
-        add = line.split(';')
-        ingredients += add
-    print(*random.sample(ingredients, 5), sep=', ')
+    f = open('ingredients2.csv', 'r')
+    ingredients = list(f.readline().split(";"))[:]
+    final_ingredients = []
+    spisok = []
+    for i in range(0, 588):
+        spisok.append(list(int(x) for x in f.readline().split(";"))[:])
+    first_ingredient = random.randint(0, 587)
+    if max(spisok[first_ingredient]) < 10:
+        while max(spisok[first_ingredient]) < 10:
+            first_ingredient = random.randint(0, 588)
+    final_ingredients.append(first_ingredient)
+    final_ingredients.append(spisok[first_ingredient].index(max(spisok[first_ingredient])))
+    #print(ingredients[first_ingredient], ingredients[final_ingredients[1]])
+    last_ingredient = final_ingredients[1]
+    while len(final_ingredients) < 5:
+        flag = True
+        add_ingredient = spisok[last_ingredient].index(max(spisok[last_ingredient]))
+        ost = spisok[last_ingredient]
+        while flag:
+            n = 0
+            for i in range(0, len(final_ingredients)):
+                if spisok[add_ingredient][final_ingredients[i]] > 0:
+                   n += 1
+            if add_ingredient in final_ingredients or n != len(final_ingredients):
+                ost[add_ingredient] = 0
+                add_ingredient = spisok[last_ingredient].index(max(spisok[last_ingredient]))
+            else:
+                flag = False
+                final_ingredients.append(spisok[last_ingredient].index(max(spisok[last_ingredient])))
+        last_ingredient = final_ingredients[-1]
+    for elem in final_ingredients:
+        print(ingredients[elem])
 
 combination()
