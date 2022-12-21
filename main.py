b538bbafc34bb2d702610b535cda6367ac99e715
ArsPro13 <Vintbolt13@gmail.com>
2022-12-21 05:06:41 +0300

    добавлены переменные

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return render_template('index.html')
+  link = "answer123.txt"
+  return render_template('index.html', link = link)
 
 @app.route('/download/<path:filename>')
 def downloading(filename):
