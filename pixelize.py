6ebb8ebef2c32a78a2060b0e4d42f5ca89266aac
semenishchev-ai <113626930+semenishchev-ai@users.noreply.github.com>
2022-12-21 13:16:06 +0300

    Pixel_size parametr

--- pixelize.py
+++ pixelize.py
@@ -2,11 +2,8 @@ import img_convert
 from PIL import Image
 
 
-def process(path, dir_name):  # path to image in input
+def process(path, dir_name, pixel_size):  # path to image in input
     image = Image.open(path)
-    pixel_size = 4  # set pixel_size
-    number_of_pins = 360  # set how many pins on lines (multiplied by pixel_size)
-    # new_image = image.resize((number_of_pins, number_of_pins * image.size[1] // image.size[0]))
     basic_colors = {(0, 0, 0): ["Black", 0], (255, 255, 0): ["Yellow", 0], (0, 255, 255): ["Cyan", 0],
                     (255, 0, 255): ["Magenta", 0], (255, 0, 0): ["Red", 0], (0, 255, 0): ["Green", 0],
                     (0, 0, 255): ["Blue", 0], (255, 255, 255): ["White", 0]}  # palette
@@ -18,4 +15,3 @@ def process(path, dir_name):  # path to image in input
     img_convert.count_pixels(new_image, basic_colors, pixel_size)
     new_path = img_convert.write_the_instruction(path, dir_name, new_image, basic_colors, pixel_size)
     return ("output.png", "instruction.pdf")
-
