2aad56ff788cc42d36e7b1de99a596fb93e250bf
afarovruslan <117665913+afarovruslan@users.noreply.github.com>
2022-12-21 16:12:34 +0300

    do app a global variable(don't work)((

--- __wrapweb__/wrapweb.py
+++ __wrapweb__/wrapweb.py
@@ -2,60 +2,78 @@ from flask import Flask, render_template, request
 import importlib
 import ast
 import os
-# import models
-# from initApp import app
+import asyncio
+from functools import wraps
 from inspect import getmembers, isfunction
 from werkzeug.utils import secure_filename
 from flask import send_from_directory
 
+
 CURRENT_PATH = os.getcwd()
 UPPER_PATH = os.path.dirname(os.getcwd())
+app = Flask(__name__)
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+
+
+def exception_handler(func):
+  def wrapper(*args, **kwargs):
+    try:
+        return func(*args, **kwargs)
+    except Exception as e:
+        error_code = getattr(e, "code", 500)
+  # Renaming the function name:
+  wrapper.__name__ = func.__name__
+  return wrapper
+
+
+def my_decorator(f):
+    @wraps(f)
+    def wrapper(*args, **kwds):
+        return f(*args, **kwds)
+    return wrapper
 
 
 def toweb(function, url_site: str, parameters: dict):
-    app = Flask(__name__)
     UPLOAD_FOLDER = f"{UPPER_PATH}/__wrapweb__/{url_site}_tmp_files"
     if not os.path.isdir(UPLOAD_FOLDER):
         os.mkdir(UPLOAD_FOLDER)
-    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-    SECRET_KEY = os.urandom(32)
-    app.config['SECRET_KEY'] = SECRET_KEY
 
     @app.route('/download/<path:filename>')
+    @my_decorator
     def downloading(filename):
-        return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+        return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
 
-    if ('account' not in parameters.keys() or not parameters['account']) and (
-            'history' not in parameters.keys() or not parameters['history']):
-        @app.route(f'/{url_site}', methods=['GET', 'POST'])
-        def myfunc():
-            result = None
-            if request.method == 'POST':
-                annotations = function.__annotations__
-                types = list(annotations.values())
-                args: list = []
-                for i in range(parameters['input']['text']):
-                    form = request.form[f'text{i}']
-                    args.append(types[i](form))
-                for i in range(parameters['input']['files']):
-                    file = request.files[f'file{i}']
-                    filename = secure_filename(file.filename)
-                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                    args.append(f"{app.config['UPLOAD_FOLDER']}/{filename}")
-                result = function(*args)
-                if result.__class__ == 'tuple':
-                    result = list(result)
-                else:
-                    result = [result]
-            return render_template('index.html', parameters=parameters, url=url_site, result=result, l=len(parameters['output']))
+    print(url_site)
 
-    return app
+    @app.route(f'/{url_site}', methods=['GET', 'POST'])
+    @my_decorator
+    def myfunc():
+        result = None
+        if request.method == 'POST':
+            annotations = function.__annotations__
+            types = list(annotations.values())
+            args: list = []
+            for i in range(parameters['input']['files']):
+                file = request.files[f'file{i}']
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(UPLOAD_FOLDER, filename))
+                args.append(f"{UPLOAD_FOLDER}/{filename}")
+            for i in range(parameters['input']['text']):
+                form = request.form.get(f'text{i}')
+                args.append(types[i](form))
+            result = function(*args)
+            if result.__class__ == 'tuple':
+                result = list(result)
+            else:
+                result = [result]
+        return render_template('index.html', parameters=parameters, url=url_site, result=result, l=len(parameters['output']))
 
 
 def wrapweb(url_site: str, name_file: str, parameters: dict):
     module = importlib.import_module(name_file[:-3])
     func = getmembers(module, isfunction)[0][1]
-    toweb(func, url_site, parameters).run(host='0.0.0.0', port=5001, debug=True)
+    toweb(func, url_site, parameters)
 
 
 def get_parameters(path_file: str) -> list:
@@ -81,9 +99,11 @@ def route_url(now_path: str):
             route_url(dir)
         for file in files:
             if file == '__init__.py':
-                file_path = f"{now_path}/{file}"
+                file_path = f"{root}/{file}"
                 for url, name_func_file, parameters in get_parameters(file_path):
                     wrapweb(url, name_func_file, parameters)
 
 
-route_url(UPPER_PATH)
+if __name__ == '__main__':
+    route_url(UPPER_PATH)
+    app.run(debug=True)
