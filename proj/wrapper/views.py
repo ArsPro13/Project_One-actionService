652dfb2f63da4a2d2ff1302e7bd88a794b88aa53
ArsPro13 <Vintbolt13@gmail.com>
2023-04-21 13:29:38 +0300

    First_commit

---
+++ proj/wrapper/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from . import services
+
+views = []
+
+def f():
+    return 1
+
+def wrap(f):
+    def my_view(request):
+       return HttpResponse(str(f()))
+    my_view.__name__ =  f.__name__
+    return my_view
+
+for n in dir(services):
+    if "_" not in n:
+        views.append(wrap(getattr(services, n)))
+
