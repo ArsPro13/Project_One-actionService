286b1640d2485217f355405e085d64c9ae55336f
sikalovaliza <116655998+sikalovaliza@users.noreply.github.com>
2023-04-25 13:21:12 +0300

    первая попытка

---
+++ myproject/myproject/myapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            message = 'Invalid login credentials'
+    else:
+        message = ''
+    return render(request, 'login.html', {'message': message})
