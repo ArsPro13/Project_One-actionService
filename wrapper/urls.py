63a48738bb179c53014bfc437582c9acaf0fcf59
ArsPro13 <67059479+ArsPro13@users.noreply.github.com>
2023-05-23 13:12:25 +0300

    done

--- wrapper/urls.py
+++ wrapper/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
 from . import views
 
+def reg(request):
+    return HttpResponseRedirect("/login")
+
 urlpatterns = [
+    path('', reg),
     path('login/', views.login_view, name='login'),
     path('home/', views.home, name='home'),
 ]
